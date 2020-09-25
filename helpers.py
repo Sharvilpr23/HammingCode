a8dddb08d09cd91f30b1d32d5e714f2e6c66d212
Sharvil Pai Raiker <sharvil.pairaiker@mines.sdsmt.edu>
2020-09-25 08:21:43 -0600

    Allowed the possibility of no error

--- helpers.py
+++ helpers.py
@@ -5,11 +5,15 @@ def generate_random_message(num_data_bits):
       for i in range(num_data_bits):
           message.append(randint(0, 1))
       return message
+
 def generate_error(x, total_bits):
-      loc = randint(1, total_bits);
-      flip_bit(x, loc)
+      chance_of_error = (randint(0, total_bits))
+      print(chance_of_error)
+      if chance_of_error > 0:  
+        loc = randint(1, total_bits);
+        flip_bit(x, loc)
       return
   
 def flip_bit(x, loc):
-      x[loc - 1] = x[loc - 1] ^ 1
+      x[loc - 1] = 1 - x[loc - 1]
       return
